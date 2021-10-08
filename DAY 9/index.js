3c60b5c290c2d7049419b8b0734d87bf09088a32
grawish <grawish@outlook.com>
2021-10-09 01:55:42 +0530

    day9

---
+++ DAY 9/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.listen(1111);
+
+const path = require("path");
+var root = __dirname;
+app.set("views", path.join(root, "/views"));
+app.set("view engine", "jade");
+app.use("/", (req, res) => {
+  res.render("content", { name: "Hello World!" });
+});
+
+app.get("/", (req, res) => {
+});
