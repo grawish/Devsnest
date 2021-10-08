d67e7c43ff91fd99f68464261c3d911943d4c4b3
grawish <grawish@outlook.com>
2021-10-09 02:19:23 +0530

    day11and12

---
+++ Day 11 and 12/database/index.js
@@ -0,0 +1,23 @@
+const {Sequelize} = require("sequelize");
+const sequelize = new Sequelize(
+    "postgres",
+    "postgres",
+    "123456789",
+    {
+        host: "localhost",
+        dialect: "postgres"
+    }
+);
+
+sequelize.sync();
+
+(async ()=>{
+    try{
+        await sequelize.authenticate();
+        console.log("connection establised with DB");
+    } catch (err) {
+        console.error("unable to connect to DB");
+    }
+})();
+
+module.exports = sequelize;
---
+++ Day 11 and 12/middlewares/registerChecks.js
@@ -0,0 +1,34 @@
+const { emailValidate, passwordValidate } = require("../utils/validate");
+const registerInitialCheck = (req, res, next) => {
+    const { email, password, confirmPassword } = req.body;
+    if (typeof email !== 'string' || typeof password !== 'string' || typeof confirmPassword !== 'string') {
+        res.status(401).send("Type Error Failed!");
+    }
+    else if (email.length < 0 || password.length < 8 || confirmPassword.length < 8) {
+        res.status(401).send("length error!");
+    }else if (confirmPassword !== password) {
+        res.status(401).send("passwords do not match!");
+    }else if (!emailValidate(email)) {
+        res.status(401).send("invalid Email: "+email);
+    }else if (!passwordValidate(password)) {
+        res.status(401).send("invalid password");
+    }else{
+        next();
+    }
+    // if (
+    //     typeof email === 'string' &&
+    //     typeof password === 'string' &&
+    //     typeof confirmpassword === 'string' &&
+    //     email.length > 0 &&
+    //     password.length > 8 &&
+    //     confirmPassword === password &&
+    //     emailValidate(email) &&
+    //     passwordValidate(password)
+    // ) {
+    //     next();
+    // }
+    // else
+    //     res.status(401).send("Initial Checks Failed!");
+}
+
+module.exports = registerInitialCheck;
